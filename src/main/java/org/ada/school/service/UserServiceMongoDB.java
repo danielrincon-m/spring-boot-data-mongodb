5b24a0dd7de7c9cc0b976bff393b1196f82635bf
Daniel Felipe Rincón Muñoz <daniel.rincon@masiv.com>
2021-08-31 20:55:30 -0500

    User Service for MongoDB Basic implementation

--- src/main/java/org/ada/school/service/UserServiceMongoDB.java
+++ src/main/java/org/ada/school/service/UserServiceMongoDB.java
@@ -1,12 +1,17 @@
 package org.ada.school.service;
 
 import org.ada.school.dto.UserDto;
-import org.ada.school.model.User;
+import org.ada.school.model.IUser;
+import org.ada.school.repository.UserDocument;
 import org.ada.school.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class UserServiceMongoDB implements UserService {
 
     private final UserRepository userRepository;
@@ -16,27 +21,38 @@ public class UserServiceMongoDB implements UserService {
     }
 
     @Override
-    public User create(UserDto user) {
-        return null;
+    public IUser create(UserDto userDto) {
+        UserDocument userDocument = new UserDocument(userDto);
+        return userRepository.save(userDocument);
     }
 
     @Override
-    public User findById(String id) {
-        return null;
+    public IUser findById(String id) {
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
-    public List<User> all() {
-        return null;
+    public List<IUser> all() {
+        return new ArrayList<>(userRepository.findAll());
     }
 
     @Override
     public boolean deleteById(String id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public User update(UserDto userDto, String id) {
+    public IUser update(UserDto userDto, String id) {
+        Optional<UserDocument> userDocumentOptional = userRepository.findById(id);
+        if (userDocumentOptional.isPresent()) {
+            UserDocument userDocument = userDocumentOptional.get();
+            userDocument.update(userDto);
+            return userRepository.save(userDocument);
+        }
         return null;
     }
 }
